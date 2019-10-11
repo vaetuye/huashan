ef127067e83cfba7850800edb35137c32c17ca65
novestoping <724460131@qq.com>
2019-10-11 14:23:05 +0800

    Teacher.java添加新内容了

--- Teacher.java
+++ Teacher.java
@@ -3,4 +3,12 @@ class Teacher{
 	private int age;
 
 	private String location;
+	public String getLocation(){
+
+	return this.location;
+	}
+			
+	public voidsetLocation(String location){
+	this.location = location ;
+}
 }
